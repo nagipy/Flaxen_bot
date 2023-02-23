9f88589afb0c83ecf45bc1564bc7eb159e4e37fd
nagi_py <95587372+nagipy@users.noreply.github.com>
2023-02-23 15:43:44 +0900

    index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Client } = require("discord.js") 
+const client = new Client()
+
+const time = new Date().toLocaleString("jp-JP", { timeZone:  "Asia/Tokyo" })
+
+client.on("ready", () =>  {
+ console.log(`login bot: ${client.user.tag}\nstart: ${time}`)
+  
+ client.user.setPresence({ status: "idle" })
+  
+  const files = require("./files")
+  files(client)
+  
+  console.log(`end: ${time}`)
+})
+
+client.login("Your bot token")
