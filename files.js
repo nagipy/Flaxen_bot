445001531ca4684699a165d2378976bfa1d393ad
nagi_py <95587372+nagipy@users.noreply.github.com>
2023-02-23 17:31:54 +0900

    files.js

---
+++ files.js
@@ -0,0 +1,21 @@
+module.exports = client => {
+  const avatar = require(`./commands/avatar`)
+  avatar(client)
+  const help = require(`./commands/help`)
+  help(client)
+  const info = require(`./commands/info`)
+  info(client)
+  const yt = require(`./commands/ys_search`)
+  yt(client)
+  
+  const lobby = require(`./logs/lobby`)
+  lobby(client)
+  const voice = require(`./logs/voice`)
+  voice(client)
+  
+  const status = require("./status")
+  status(client)
+  
+  const rpc = require("./rpc")
+  rpc(client)
+}
